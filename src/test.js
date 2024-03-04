164384993e506a8c66d87736fa781ef8a45e5c0b
fayaz-rafin <fayaz.rafin@gmail.com>
2024-03-04 00:07:09 -0500

    successfully added slash commands

--- src/test.js
+++ src/test.js
@@ -2,7 +2,6 @@ require('dotenv/config');
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
